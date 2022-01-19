fc43502451cc1c3daed59d8d97fb4adf0949bc4b
Nick Hodson <hodson.n@etoncollege.org.uk>
2022-01-19 10:06:12 +0000

    Initial Commit

---
+++ NewTamagotchi/NewTamagotchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewTamagotchiApp.swift
+//  NewTamagotchi
+//
+//  Created by Nicholas Hodson on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewTamagotchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
