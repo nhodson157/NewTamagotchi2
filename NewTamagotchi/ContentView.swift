f2d733568526d130886a3103dc577c1cda90ff9e
Nick Hodson <hodson.n@etoncollege.org.uk>
2022-01-19 10:22:16 +0000

    update

--- NewTamagotchi/ContentView.swift
+++ NewTamagotchi/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, universe")
+        Text("Hello, universe - pleased to meet you")
             .padding()
     }
 }
